95228c60490ddaf883fb2daa678e8e4035db4432
waverDeep <dandyleader@naver.com>
2019-02-28 16:59:49 +0900

    activity_study.xml  -> 51번째줄 marginTop : 20dp 로 수정
     관련 액티비티 생성 및 1차 xml 디자인 완성하였습니다.

--- app/src/main/java/com/goodset/younmin/youngminjeungeum/mainFragment/InfoFragment.java
+++ app/src/main/java/com/goodset/younmin/youngminjeungeum/mainFragment/InfoFragment.java
@@ -1,5 +1,6 @@
 package com.goodset.younmin.youngminjeungeum.mainFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,18 +8,71 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.goodset.younmin.youngminjeungeum.AppInfoActivity;
+import com.goodset.younmin.youngminjeungeum.OpenLicenseActivity;
 import com.goodset.younmin.youngminjeungeum.R;
 
 public class InfoFragment extends Fragment {
 
     private ViewGroup root;
 
+    private ImageView ivInfoBackground;
+    private TextView tvInfoText;
+    private ImageView ivLicenseBackground;
+    private TextView tvLicenseText;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root=(ViewGroup)inflater.inflate(R.layout.fragment_info,container,false);
 
+        initView();
+        initInstance();
+        eventListener();
+
         return root;
     }
+
+    private void initView(){
+        ivInfoBackground=root.findViewById(R.id.ivInfoBackground);
+        tvInfoText=root.findViewById(R.id.tvInfoText);
+        ivLicenseBackground=root.findViewById(R.id.ivLicenseBackground);
+        tvLicenseText=root.findViewById(R.id.tvLicenseText);
+
+
+    }
+
+    private void initInstance(){
+
+
+    }
+
+    private void eventListener(){
+        ivInfoBackground.setOnClickListener(iconListener);
+        tvInfoText.setOnClickListener(iconListener);
+        ivLicenseBackground.setOnClickListener(iconListener);
+        tvLicenseText.setOnClickListener(iconListener);
+
+    }
+
+    private View.OnClickListener iconListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.ivInfoBackground:
+                case R.id.tvInfoText:
+                    Intent infoIntent=new Intent(getActivity(), AppInfoActivity.class);
+                    startActivity(infoIntent);
+                    break;
+                case R.id.ivLicenseBackground:
+                case R.id.tvLicenseText:
+                    Intent openLicenseIntent=new Intent(getActivity(), OpenLicenseActivity.class);
+                    startActivity(openLicenseIntent);
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/com/goodset/younmin/youngminjeungeum/mainFragment/QuizFragment.java
+++ app/src/main/java/com/goodset/younmin/youngminjeungeum/mainFragment/QuizFragment.java
@@ -1,5 +1,6 @@
 package com.goodset.younmin.youngminjeungeum.mainFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,18 +8,69 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.goodset.younmin.youngminjeungeum.QuizActivity;
+import com.goodset.younmin.youngminjeungeum.QuizStatActivity;
 import com.goodset.younmin.youngminjeungeum.R;
 
 public class QuizFragment extends Fragment {
 
     private ViewGroup root;
+    private ImageView ivQuizBackground;
+    private TextView tvQuizText;
+    private ImageView ivQuizStatBackground;
+    private TextView tvQuizStatText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root=(ViewGroup)inflater.inflate(R.layout.fragment_quiz,container,false);
 
+        initView();
+        initInstance();
+        eventListener();
+
+
         return root;
     }
+
+    private void initView(){
+        ivQuizBackground=root.findViewById(R.id.ivQuizBackground);
+        ivQuizStatBackground=root.findViewById(R.id.ivQuizStatBackground);
+        tvQuizText=root.findViewById(R.id.tvQuizText);
+        tvQuizStatText=root.findViewById(R.id.tvQuizStatText);
+    }
+
+    private void initInstance(){
+
+
+    }
+
+    private void eventListener(){
+        ivQuizStatBackground.setOnClickListener(iconListener);
+        tvQuizStatText.setOnClickListener(iconListener);
+        ivQuizBackground.setOnClickListener(iconListener);
+        tvQuizText.setOnClickListener(iconListener);
+
+    }
+
+    private View.OnClickListener iconListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.ivQuizBackground:
+                case R.id.tvQuizText:
+                    Intent quizIntent =new Intent(getActivity(), QuizActivity.class);
+                    startActivity(quizIntent);
+                    break;
+                case R.id.ivQuizStatBackground:
+                case R.id.tvQuizStatText:
+                    Intent quizStatIntent =new Intent(getActivity(), QuizStatActivity.class);
+                    startActivity(quizStatIntent);
+                    break;
+            }
+        }
+    };
 }
